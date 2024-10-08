8e2bfb8d0cecebe839ab5a480d9090d78b9d3c8b
Christopher Cortes <christophercr@gmail.com>
2024-10-08 11:03:30 +0200

    sesion-2: implementar tests de inputs y outputs de NewMediaCollection

--- media-manager-ng/src/app/components/new-media-collection/new-media-collection.component.spec.ts
+++ media-manager-ng/src/app/components/new-media-collection/new-media-collection.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NewMediaCollectionComponent } from './new-media-collection.component';
+import { BookService } from '../../services/book.service';
 
 describe('NewMediaCollectionComponent', () => {
   let component: NewMediaCollectionComponent;
   let fixture: ComponentFixture<NewMediaCollectionComponent>;
 
   beforeEach(async () => {
+    const bookServiceMock = jasmine.createSpyObj('BookService', ['createBookCollection']);
+    
     await TestBed.configureTestingModule({
       imports: [NewMediaCollectionComponent],
+      providers: [
+        {
+          provide: BookService,
+          useValue: bookServiceMock,
+        }
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NewMediaCollectionComponent);
@@ -16,7 +25,29 @@ describe('NewMediaCollectionComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('inputs:', () => {
+    describe('dado el input "inputNname"', () => {
+      it('debe de poner el valor en la caja de texto del nombre de la colección', () => {
+        component.inputName = 'otro valor';
+        const compiledHtml = fixture.nativeElement as HTMLElement;
+        const inputBox = compiledHtml.querySelector('[data-test="field-collection-name"]') as HTMLInputElement;
+        expect(inputBox.value).toBe('otro valor');
+      });
+    });
+  });
+
+  describe('outputs:', () => {
+    describe('reloadClicked', () => {
+      it('debe emitir un evento cuando el botón "Reload collections" es clickado', () => {
+        const spy = spyOn(component.reloadClicked, 'emit');
+
+        const compiledHtml = fixture.nativeElement as HTMLElement;
+        const button = compiledHtml.querySelector('[data-test="btn-reload-collections"]') as HTMLButtonElement;
+        button.click();
+
+        expect(spy).toHaveBeenCalledTimes(1);
+        expect(spy).toHaveBeenCalledWith();
+      });
+    });
   });
 });
