2079c8ed892f1ec0f739b5bc0362932955f939c0
davidof1977 <davidof1977@hotmail.com>
2024-10-14 12:47:10 +0200

    Sesion-5: Test de directiva if-viewport

--- media-manager-ng/src/app/directives/if-viewport.directive.spec.ts
+++ media-manager-ng/src/app/directives/if-viewport.directive.spec.ts
@@ -1,8 +1,68 @@
+import { Component } from '@angular/core';
 import { IfViewportDirective } from './if-viewport.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { BehaviorSubject, of, Subject } from 'rxjs';
 
-xdescribe('IfViewportDirective', () => {
-  it('should create an instance', () => {
-    const directive = new IfViewportDirective();
-    expect(directive).toBeTruthy();
+describe('IfViewportDirective', () => {
+  @Component({
+    standalone: true,
+    template: `
+      <h1>Testing IfViewportDirective...</h1>
+      <div *appIfViewport="'Large'; else otherContent">
+        <p data-test="content-when-match">Shoud be shown when the viewport matches</p>
+      </div>
+
+      <ng-template #otherContent>
+        <div data-test="content-when-no-match">Should be shown when the viewport does not match</div>
+      </ng-template>
+    `,
+    imports: [IfViewportDirective],
+  })  class TestIfViewportDirectiveComponent {}
+
+  let component: TestIfViewportDirectiveComponent;
+  let fixture: ComponentFixture<TestIfViewportDirectiveComponent>;
+  let observerService: jasmine.SpyObj<BreakpointObserver>;
+  const subject = new BehaviorSubject<BreakpointState>({breakpoints:{
+    [Breakpoints.Large]:true
+  },matches:true});
+  const observerServiceMock = jasmine.createSpyObj('BreakpointObserver', ['observe']);
+
+  beforeEach(async () => {
+
+    await TestBed.configureTestingModule({
+      imports: [TestIfViewportDirectiveComponent, IfViewportDirective],
+      providers: [
+        {
+          provide: BreakpointObserver,
+          useValue: observerServiceMock,
+        },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestIfViewportDirectiveComponent);
+    component = fixture.componentInstance;
+    observerService = TestBed.inject(BreakpointObserver) as jasmine.SpyObj<BreakpointObserver>;
+    observerService.observe.and.returnValue(subject.asObservable());
+    fixture.detectChanges();
+
+  });
+  it('debe mostrar las vista correspondiente en caso de que el viewport sea el deseado y en el caso en el que no', () => {
+
+    const html = fixture.nativeElement as HTMLElement;
+    let parrafoMatch = html.querySelector('[data-test="content-when-match"]') as HTMLParagraphElement;
+    expect(parrafoMatch.innerHTML).toBe('Shoud be shown when the viewport matches');
+    subject.next({breakpoints:{
+      [Breakpoints.Large]:false,
+      [Breakpoints.Medium]:true
+    },matches:true})
+    const parrafoNotMatch = html.querySelector('[data-test="content-when-no-match"]') as HTMLParagraphElement;
+    expect(parrafoNotMatch.innerHTML).toBe('Should be shown when the viewport does not match');
+    subject.next({breakpoints:{
+      [Breakpoints.Large]:true,
+      [Breakpoints.Medium]:false
+    },matches:true})
+    parrafoMatch = html.querySelector('[data-test="content-when-match"]') as HTMLParagraphElement;
+    expect(parrafoMatch.innerHTML).toBe('Shoud be shown when the viewport matches');
   });
 });
