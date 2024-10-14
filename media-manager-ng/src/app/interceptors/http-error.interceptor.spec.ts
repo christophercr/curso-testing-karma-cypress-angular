b4e37e98f4cd1b41d2a14f90a68c8435a1bafeb7
Christopher Cortes <christophercr@gmail.com>
2024-10-14 13:12:05 +0200

    sesion-5: implementar tests del Http Error Interceptor

--- media-manager-ng/src/app/interceptors/http-error.interceptor.spec.ts
+++ media-manager-ng/src/app/interceptors/http-error.interceptor.spec.ts
@@ -1,9 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpEventType, HttpInterceptorFn, HttpRequest, HttpResponse, type HttpHandlerFn } from '@angular/common/http';
 
 import { httpErrorInterceptor } from './http-error.interceptor';
+import { of, throwError } from 'rxjs';
 
-xdescribe('httpErrorInterceptor', () => {
+fdescribe('httpErrorInterceptor', () => {
   const interceptor: HttpInterceptorFn = (req, next) => TestBed.runInInjectionContext(() => httpErrorInterceptor(req, next));
 
   beforeEach(() => {
@@ -12,5 +13,47 @@ xdescribe('httpErrorInterceptor', () => {
 
   it('should be created', () => {
     expect(interceptor).toBeTruthy();
+  });
+
+  it('debe de re-lanzar un error en caso de que la peticion Http falle', (done: DoneFn) => {
+    const dummyRequest = new HttpRequest('GET', '/some/url');
+
+    const dummyHandlerErrorFn: HttpHandlerFn = (req: HttpRequest<unknown>) => {
+      const failureResponse = new HttpErrorResponse({ error: 'response ERROR', status: 500 });
+
+      return throwError(() => failureResponse);
+    };
+
+    interceptor(dummyRequest, dummyHandlerErrorFn).subscribe({
+      next: () => {
+        fail('No debe de emitir nada en next() en caso de que la petición Http falle');
+      },
+      error: (error) => {
+        expect(error).toBeDefined();
+        expect(error).toMatch(/HTTP Error interceptor: Http call failed due to error/);
+        done();
+      },
+    });
+  });
+
+  it('no debe hacer nada con la respuesta en caso de que la peticion Http sea exitosa', (done: DoneFn) => {
+    const dummyRequest = new HttpRequest('GET', '/some/url');
+
+    const dummyHandlerErrorFn: HttpHandlerFn = (req: HttpRequest<unknown>) => {
+      const successResponse = new HttpResponse({ body: 'response SUCCESS', status: 200 });
+
+      return of(successResponse);
+    };
+
+    interceptor(dummyRequest, dummyHandlerErrorFn).subscribe({
+      next: (response) => {
+        expect(response).toBeDefined();
+        expect(response.type).toBe(HttpEventType.Response);
+        done();
+      },
+      error: () => {
+        fail('No debe de haber error si la petición Http es exitosa');
+      },
+    });
   });
 });
